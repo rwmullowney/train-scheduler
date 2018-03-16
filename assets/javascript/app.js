191cc0d4df9576ee834b06d472d7f64376da6214
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-03-16 13:12:36 -0500

    Adds time calculation code, but for some reason doesn't work with displaying to page (does work in console though)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,81 @@
 (function () {
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyD7Z2mCLTJvdDie5SQfyu5VOq3yJAMYMu0",
-    authDomain: "train-scheduler-41cc0.firebaseapp.com",
-    databaseURL: "https://train-scheduler-41cc0.firebaseio.com",
-    projectId: "train-scheduler-41cc0",
-    storageBucket: "train-scheduler-41cc0.appspot.com",
-    messagingSenderId: "433615014625"
-};
-firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyD7Z2mCLTJvdDie5SQfyu5VOq3yJAMYMu0",
+        authDomain: "train-scheduler-41cc0.firebaseapp.com",
+        databaseURL: "https://train-scheduler-41cc0.firebaseio.com",
+        projectId: "train-scheduler-41cc0",
+        storageBucket: "train-scheduler-41cc0.appspot.com",
+        messagingSenderId: "433615014625"
+    };
+    firebase.initializeApp(config);
 
 
-// Variable declarations
-var trainName;
-var destination;
-var firstTrain;
-var frequency;
-var nextArrival;
-var minutesAway;
-var database = firebase.database();
+    // Variable declarations
+    var trainName;
+    var destination;
+    var firstTrain;
+    var firstTrainConverted;
+    var frequency;
+    var nextArrival;
+    var minutesAway;
+    var currentTime;
+    var diffTime;
+    var remainder;
+    var nextTrain;
+    var database = firebase.database();
 
 
-// Empties the user input areas
-function emptyInput() {
-    $("#trainName").val('');
-    $("#destination").val('');
-    $("#firstTrain").val('');
-    $("#frequency").val('');
-}
+    // Empties the user input areas
+    function emptyInput() {
+        $("#trainName").val('');
+        $("#destination").val('');
+        $("#firstTrain").val('');
+        $("#frequency").val('');
+    }
 
-// When Submit button is clicked
-$("#submit").on('click', function () {
+    // When Submit button is clicked
+    $("#submit").on('click', function () {
 
-    // Assigns user input to variables
-    trainName = $("#trainName").val();
-    destination = $("#destination").val();
-    firstTrain = $("#firstTrain").val();
-    frequency = $("#frequency").val();
+        // Assigns user input to variables
+        trainName = $("#trainName").val();
+        destination = $("#destination").val();
+        firstTrain = $("#firstTrain").val();
+        frequency = $("#frequency").val();
+        // Calculate arrival time & minutes away
+        // First Train (pushed back 1 year to make sure it comes before current time)
+        firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+        currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        // Difference between the times
+        diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+        // Time apart (remainder)
+        remainder = diffTime % frequency;
+        console.log(remainder);
+        // Minute Until Train
+        minutesAway = frequency - remainder;
+        console.log("MINUTES TILL TRAIN: " + minutesAway);
+        // Next Train
+        nextTrain = moment().add(minutesAway, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    emptyInput();
-    console.log(trainName, destination, firstTrain, frequency)
 
-    // Code for handling the push
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-})
+        emptyInput();
+        // console.log(trainName, destination, firstTrain, frequency)
+
+        // Code for handling the push
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            minutesAway: minutesAway,
+            nextTrain: nextTrain,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+    })
 
 
     // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
@@ -60,17 +85,19 @@ $("#submit").on('click', function () {
             // storing the snapshot.val() in a variable for convenience
             var sv = snapshot.val();
 
-            // Console.loging the last user's data
-            console.log(sv.trainName);
-            console.log(sv.destination);
-            console.log(sv.firstTrain);
-            console.log(sv.frequency);
+            // // Console.loging the last user's data
+            // console.log(sv.trainName);
+            // console.log(sv.destination);
+            // console.log(sv.firstTrain);
+            // console.log(sv.frequency);
 
             var infoRow = $("<tr>")
             infoRow.append("<th>" + sv.trainName + "</th>")
             infoRow.append("<td>" + sv.destination + "</td>")
             infoRow.append("<td>" + sv.firstTrain + "</td>")
             infoRow.append("<td>" + sv.frequency + "</td>")
+            // infoRow.append("<td>" + sv.nextTrain + "</td>")
+            // infoRow.append("<td>" + sv.minutesAway + "</td>")
             // infoRow.append("<td>$" + sv.rate + "</td>")
             // infoRow.append("<td>$" + billed + "</td>")
             $("#trainInfo").append(infoRow)
